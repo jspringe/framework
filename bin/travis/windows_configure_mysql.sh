02f7592a3efb581c5065b36019a7d72f48296f00
Joe Springe <518362+jspringe@users.noreply.github.com>
2019-08-21 18:47:15 -0400

    Fix MySQL declaration

--- bin/travis/windows_configure_mysql.sh
+++ bin/travis/windows_configure_mysql.sh
@@ -1,2 +1,2 @@
-/c/tools/mysql/bin/mysql.exe -u root --skip-password -e "SET PASSWORD FOR root@localhost = PASSWORD('');";
-/c/tools/mysql/bin/mysql.exe -u root -e 'CREATE DATABASE forge;';
+/c/tools/mysql/current/bin/mysql.exe -u root --skip-password -e "SET PASSWORD FOR root@localhost = PASSWORD('');";
+/c/tools/mysql/current/bin/mysql.exe -u root -e 'CREATE DATABASE forge;';
