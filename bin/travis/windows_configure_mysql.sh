3e56c2c05def32f2ee7c4aa9b867dbcb3f17135d
Joe Springe <518362+jspringe@users.noreply.github.com>
2019-08-21 19:03:56 -0400

    Add CREATE db to main path

--- bin/travis/windows_configure_mysql.sh
+++ bin/travis/windows_configure_mysql.sh
@@ -1,2 +1,1 @@
 /c/tools/mysql/current/bin/mysql.exe -u root --skip-password -e "SET PASSWORD FOR root@localhost = PASSWORD('');";
-/c/tools/mysql/current/bin/mysql.exe -u root -e 'CREATE DATABASE forge;';
